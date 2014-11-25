e6a09826a3001adbbb93764b79a070ecb3b429f9
Yawman, Chris <christopher.yawman@neustar.biz>
2014-11-25 13:41:52 -0500

    Added route, view and layout for dashboard

--- config/module.config.php
+++ config/module.config.php
@@ -1,6 +1,34 @@
 <?php
 
 return array(
+    'router' => array(
+        'routes' => array(
+            'admin' => array(
+                'type' => 'Zend\Mvc\Router\Http\Literal',
+                'options' => array(
+                    'route' => '/admin',
+                    'defaults' => array(
+                        'controller' => 'UghAdministration\Controller\Dashboard',
+                        'action' => 'index'
+                    )
+                )
+            )
+        )
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'UghAdministration\Controller\Dashboard' => 'UghAdministration\Controller\DashboardController'
+        )
+    ),
+    'view_manager' => array(
+        'template_map' => array(
+            'layout/admin' => __DIR__ . '/../view/layout/admin.phtml',
+            'ugh-administration/dashboard/index' => __DIR__ . '/../view/ugh-administration/dashboard/index.phtml'
+        ),
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
     'ugh_administration' => array(
     )
 );
